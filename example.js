74883f91ba2954f63461c3f695c811b23ad574e1
VinceZK <vincezklee@gmail.com>
2016-03-11 21:05:57 +0800

    Update Phantomjs to 2.1 support pdf links

--- example.js
+++ example.js
@@ -6,20 +6,20 @@ var fs = require('fs');
 
 var render = phantom({
     format      : 'pdf',
-//    quality     : 50,
-//    retries     : 2,
-//    phantomFlags: ['--ignore-ssl-errors=true','--output-encoding=gb2312'],
+    quality     : 50,
+    retries     : 2,
+    phantomFlags: ['--ignore-ssl-errors=true','--output-encoding=gb2312'],
 //    phantomFlags: ['--ignore-ssl-errors=true', '--proxy=127.0.0.1:7070', '--proxy-type=socks5'],
     timeout     : 300000,
-//    printMedia  : true
- //   userAgent   : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'
+    printMedia  : true,
+    userAgent   : 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2062.120 Safari/537.36'
 });
 
 render('http://c7e56444.i9p.co/forumdisplay.php?fid=19', {
-    width       : 1920,
-    height      : 1080,
-//    pagerSize   : { width: 1920, height: 1080, margin: '0px' },
-    paperFormat : 'A4',        // Defaults to A4. Also supported: 'A3', 'A4', 'A5', 'Legal', 'Letter', 'Tabloid'.
+    width       : 1024,
+    height      : 768,
+    //paperSize   : { width: 1240, height: 1080, margin: '0px' },
+    //paperFormat : 'A4',        // Defaults to A4. Also supported: 'A3', 'A4', 'A5', 'Legal', 'Letter', 'Tabloid'.
     orientation : 'portrait',  // Defaults to portrait. 'landscape' is also valid
     margin      : '1cm'
 
